744f991ff6fe161e4688c170b02f32de09050fdf
dzflynn <U2012116797>
2020-05-30 16:40:20 -0500

    Poup UI, PopUp script

---
+++ Assets/__Project Assets/Scripts/Main/Sebastian Scripts/PopUpButton.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpButton : MonoBehaviour
+{
+    public GameObject popUpPanel;
+    
+    public void ClosePopUP()
+    {
+        //popUpPanel.SetActive(false);
+        //or an animation
+        Debug.Log("Poup Closed");
+    }
+}
